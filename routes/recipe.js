67f90b75c09fe744275c975720dfeae62f5baea5
KrishnaP <kpat132@gmail.com>
2018-03-13 18:50:46 -1000

    routes

---
+++ routes/recipe.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../server/db/models/User');
+
+// let url = 
+
+// router.route('/')
+
+module.exports = Router;
