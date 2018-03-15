d1bc02d6476b3f1975c344abec7535f7fe64d069
Zubin Dodhi <zubindodhi@Zubins-MacBook-Pro.local>
2018-03-14 14:33:14 -1000

    BolierPlate Setup

---
+++ src/actions/ItemsAction.js
@@ -0,0 +1,41 @@
+import "axios";
+
+const EDAMAM = "https://developer.edamam.com/edamam-docs-recipe-api";
+
+export const LOAD_RECIPES = "LOAD_RECIPES";
+export const SET_ACTIVE_RECIPES = "LOAD_RECIPES";
+
+export const setActiveRecipe = recipe => {
+  if (recipe) {
+    return {
+      type: SET_ACTIVE_RECIPES,
+      recipe
+    };
+  }
+};
+
+export const loadRecipes = () => {
+  return dispatch => {
+    return fetch(`${EDAMAM}/recipes`)
+      .then(res => {
+        return res.json();
+      })
+      .then(json => {
+        return json.results;
+      })
+      .then(recipes => {
+        dispatch({
+          type: LOAD_RECIPES,
+          recipes
+        });
+        dispatch(setActiveRecipe(recipes[0]));
+      })
+      .catch(err => {
+        console.log(err);
+        return dispatch({
+          type: LOAD_RECIPES,
+          recipes: []
+        });
+      });
+  };
+};
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,8 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+import { loadRecipes, setActiveRecipe } from "../../actions";
+import ActiveRecipe from "../../components/ActiveRecipe";
+import RecipesList from "../../components/RecipeList";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.recipeClickHandler = this.recipeClickHandler.bind(this);
+    this.findRecipeByName = this.findRecipeByName.bind(this);
+  }
+  componentDidMount() {
+    this.props.loadRecipe();
+  }
+
+  recipeClickHandler(event) {
+    const recipeName = event.currentTarget.dataset.recipeName;
+    if (recipeName) {
+      const foundRecipe = this.findRecipeByName(recipeName);
+      if (foundRecipe) {
+        this.props.setActiveRecipe(foundRecipe);
+      }
+    }
+  }
+
+  findRecipeByName(name) {
+    const foundRecipe = this.props.recipes.find(recipe => {
+      return recipe.name === name;
+    });
+    if (foundRecipe) {
+      return foundRecipe;
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -10,12 +44,17 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">FINAL PROJECT WOO</h1>
         </header>
+        <ActiveRecipe planet={this.props.activeRecipe} />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <RecipesList
+          planets={this.props.recipes}
+          recipeClickHandler={this.recipeClickHandler}
+        />
       </div>
     );
   }
 }
 
-export default App;
+export default connect(mapStateToProp, mapDispatchToProps)(App);
