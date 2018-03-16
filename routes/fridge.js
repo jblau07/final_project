f03fe929267bd65c7b5e6fe5521abcb7683eee0d
KrishnaP <kpat132@gmail.com>
2018-03-16 12:28:28 -1000

    Able to generate recipes when passing in array of ingredients

--- routes/fridge.js
+++ routes/fridge.js
@@ -6,23 +6,8 @@ const Fridge = require('../server/db/models/Fridge');
 const User = require('../server/db/models/User');
 const Ingredient = require('../server/db/models/Ingredient');
 
-router.route('/:userId')
-  .get((req, res) => {
-    let user_id = req.params.userId;
-    console.log('SOMETHING', user_id)
-    return new Fridge()
-      .where({ user_id: user_id })
-      .fetchAll({ withRelated: ['users','ingredients'] })
-      .then(usersIngr => {
-        return res.json(usersIngr.toJSON())
-      })
-      .catch(err => {
-        console.log({ err: err.message });
-        return res.json({ err: err.message });
-      })
-  })
 router.route('/:userId/:ingredientId')
-
+//Delete Ingredient from Users Fridge
   .delete((req, res) => {
     let user_id = req.params.userId;
     let ingr_id = req.params.ingredientId;
@@ -31,6 +16,22 @@ router.route('/:userId/:ingredientId')
       .destroy()
       .then(result => {
         console.log('Deleted!');
+      })
+      .catch(err => {
+        console.log({ err: err.message });
+        return res.json({ err: err.message });
+      })
+  })
+router.route('/:userId')
+//Get all Ingredients in Fridge Associated with specific User
+  .get((req, res) => {
+    let user_id = req.params.userId;
+    console.log('SOMETHING', user_id)
+    return new Fridge()
+      .where({ user_id: user_id })
+      .fetchAll({ withRelated: ['users','ingredients'] })
+      .then(usersIngr => {
+        return res.json(usersIngr.toJSON())
       })
       .catch(err => {
         console.log({ err: err.message });
