c2e0d06ff91102d7d5ebf556c7f48f3ecaaa0ed3
jblau07 <jlau7248@gmail.com>
2018-03-16 18:44:09 -1000

    added fridge reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import recipesReducer from "./recipesReducer";
 import suggestReducer from "./suggestReducer";
+import fridgeReducer from "./fridgeReducer";
+
 
 export default combineReducers({
   recipes: recipesReducer,
-  suggest: suggestReducer
-
+  suggest: suggestReducer,
+  fridge: fridgeReducer
 });
