3dce59032ec9977af8ec2205dfd443f9c530364b
jblau07 <jlau7248@gmail.com>
2018-03-17 11:15:30 -1000

    add ingredient action now posts to ingredients, fridge, and loads fridge

--- src/actions/SuggestAction.js
+++ src/actions/SuggestAction.js
@@ -2,13 +2,14 @@ import axios from 'axios';
 
 export const LOAD_INGREDIENTS = 'LOAD_INGREDIENTS';
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+export const ADD_TO_FRIDGE = 'ADD_TO_FRIDGE';
+
 
 export const loadIngredients = () => {
-  console.log('ggggg')
   return dispatch => {
     return axios.get('/api/ingredients')
     .then(data => {
-      console.log('data', data.data)
+      console.log('datawwwww', data.data)
       dispatch({
         type: LOAD_INGREDIENTS,
         ingredients: data
@@ -20,18 +21,44 @@ export const loadIngredients = () => {
   }
 }
 
+
+// export const addToFridge = (newIngredient) => {
+//   return dispatch => {
+//     return axios.post('/api/fridge', {
+//       name: newIngredient
+//     })
+//     .then(ingredient => {
+//       dispatch({
+//         type: ADD_TO_FRIDGE,
+//         singleIngredient: ingredient.data
+//       })
+    
+//     })
+//     .catch((err) => {
+//       console.log(err)
+//     })
+//   }
+// }
+
 export const addIngredient = (newIngredient) => {
-  console.log('adding')
   return dispatch => {
     return axios.post('/api/ingredients', {
       name: newIngredient
     })
     .then(ingredient => {
-      dispatch({
-        type: ADD_INGREDIENT,
-        singleIngredient: ingredient.data
+      let newFridgeItem = ingredient.data.id
+      return axios.post('/api/fridge', {
+        newFridgeItem
       })
-    
+      .then(data => {
+        return axios.get('/api/fridge/1')
+        .then(data => {
+          dispatch({
+            type: 'LOAD_FRIDGE',
+            fridge: data
+          })
+        })
+      })
     })
     .catch((err) => {
       console.log(err)
