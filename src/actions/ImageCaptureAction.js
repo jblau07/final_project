f926e49da01f87647e755bc8081c97cf8217bfe4
Levi Porter <levi@levibrooke.com>
2018-03-17 14:47:07 -1000

    Added post to server to handle sending image

--- src/actions/ImageCaptureAction.js
+++ src/actions/ImageCaptureAction.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
 
+export const SEND_IMAGE = 'SEND_IMAGE';
+
 export const sendImage = (image) => {
-  
+  return dispatch => {
+    return axios.post('/api/image-capture', {
+      image: image
+    })
+    .then(data => {
+      console.log('data', data);
+      dispatch({
+        type: SEND_IMAGE,
+        image: data.image
+      });
+    })
+    .catch(err => {
+      console.log('err', err);
+    });
+  }
 }
