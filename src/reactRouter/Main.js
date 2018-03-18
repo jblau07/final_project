873aa72cade22090557acd3bd0dee8592b5696eb
Zubin R Dodhi <32475171+DODHI5@users.noreply.github.com>
2018-03-17 15:25:55 -1000

    Pulled development (#29)
    * installed localtunnel however didnt implement it created second config file with api keys
    
    * add readme for second config file

--- src/reactRouter/Main.js
+++ src/reactRouter/Main.js
@@ -2,15 +2,15 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Fridge from "../containers/Fridge";
 import BarcodeScanner from "../containers/BarcodeScanner";
-import Register from '../containers/registerContainer';
-import Login from '../containers/LoginContainer';
+import Register from "../containers/registerContainer";
+import Login from "../containers/LoginContainer";
 const Main = props => (
   <main className="page-content">
     <Switch>
       <Route exact path="/" component={Fridge} />
-      <Route exact path="/barcodeScanner" component={BarcodeScanner} />
-      <Route exact path='/register' component={Register} />
-      <Route exact path='/login' component={Login} />
+      <Route exact path="/scan" component={BarcodeScanner} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/login" component={Login} />
     </Switch>
   </main>
 );
