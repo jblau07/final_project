980e075dc394a101d256a9287b1729217519eefe
jblau07 <jlau7248@gmail.com>
2018-03-18 15:33:38 -1000

    code update

--- src/components/FridgeList.js
+++ src/components/FridgeList.js
@@ -1,74 +1,26 @@
-// import React, {Component} from 'react';
-// import {connect} from 'react-redux';
-// import { selectFromFridge } from "../actions/FridgeAction";
+import React, {Component} from 'react';
+import FridgeIngredient from '../containers/FridgeIngredient';
 
-// class FridgeList extends Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
+export const FridgeList = ({fridge}) => {
+  let data;
+  if (fridge.length === 0) {
+    return (
+      <div></div>
+    )
+  } else {
+    data = fridge.data;
+    return (
+      <ul className="userFridge">
+        {data.map((ingredient) => {
+          return (
+            <FridgeIngredient
+              key={ingredient.id}
+              {...ingredient}/>
+          )
+        })}
+            </ul>
+    );
+  }
+}
 
-//     }
-//   }
-// }
-
-// handleOnChange(event) {
-
-// }
-
-
-
-
-
-// const mapStateToProps = state => {
-//   return {
-//     selected: state.fridge.selected
-//   }
-// }
-
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     selectFromFridge: function (name)  {
-//       dispatch(selectFromFridge(name))
-//     }
-
-//   }
-// }
-
-
-// export default connect(mapStateToProps, mapDispatchToProps)(FridgeList)
-
-
-
-
-
-
-
-
-
-// export const FridgeList = ({fridge}) => {
-//   let data;
-//   if (fridge.length === 0) {
-//     return (
-//       <div></div>
-//     )
-//   } else {
-//     data = fridge.data;
-
-//       return (
-//         <ul className="userFridge">
-//           {data.map((element,idx) => {
-//             return (
-//               <div className="ingredient_name">
-//                 <p>{element.ingredients.name}</p>
-//                 <button>Select</button>
-//               </div>
-//             )
-//           })}
-//               </ul>
-//       );
-
-
-//   }
-// }
-
-// export default FridgeList;
+export default FridgeList;
