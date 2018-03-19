0bb75893c8e6c22f8db36985130712cfca2c888e
jblau07 <jlau7248@gmail.com>
2018-03-18 15:32:50 -1000

    added user_id to load_fridge, created delete from fridge action

--- src/actions/FridgeAction.js
+++ src/actions/FridgeAction.js
@@ -3,9 +3,13 @@ import axios from 'axios';
 export const LOAD_FRIDGE = 'LOAD_FRIDGE';
 export const SELECT_FROM_FRIDGE = 'SELECT_FROM_FRIDGE';
 
-export const loadFridge = (id) => {
+export const loadFridge = () => {
+  let user_id;
+  if (localStorage.length > 0) {
+    user_id = localStorage.getItem('id');
+  }
   return dispatch => {
-    return axios.get(`/api/fridge/${id}`)
+    return axios.get(`/api/fridge/${user_id}`)
     .then(data => {
       dispatch({
         type: LOAD_FRIDGE,
@@ -18,7 +22,33 @@ export const loadFridge = (id) => {
   }
 }
 
+export const deleteFromFridge = (id) => {
+  let user_id;
+  if (localStorage.length > 0) {
+    user_id = localStorage.getItem('id');
+  }
+
+  console.log('user_id', user_id)
+  return dispatch => {
+    return axios.delete(`/api/fridge/1/${id}`)
+    .then(() => {
+      return axios.get(`/api/fridge/${user_id}`)
+        .then(data => {
+          dispatch({
+            type: 'LOAD_FRIDGE',
+            fridge: data
+          })
+        })
+        
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+}
+
 export const selectFromFridge = (name) => {
+  console.log('select', name)
   return  {
     type: SELECT_FROM_FRIDGE,
     selected: name
