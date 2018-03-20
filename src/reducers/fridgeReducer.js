e4b4dec79fb950fc663c271a1cf323b85a1c6d0f
jblau07 <jlau7248@gmail.com>
2018-03-20 10:46:16 -1000

    added clear all and deselect to reducer

--- src/reducers/fridgeReducer.js
+++ src/reducers/fridgeReducer.js
@@ -1,4 +1,4 @@
-import { LOAD_FRIDGE, SELECT_FROM_FRIDGE } from '../actions/FridgeAction';
+import { LOAD_FRIDGE, SELECT_FROM_FRIDGE, DESELECT_FROM_FRIDGE, CLEAR_SELECTED } from '../actions/FridgeAction';
 
 const initialState = {
   fridge: [],
@@ -9,9 +9,20 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case LOAD_FRIDGE:
       return {...state, fridge: action.fridge};
+    case CLEAR_SELECTED: {
+      return {...state, selected: []}
+    }
     case SELECT_FROM_FRIDGE:
       const updatedSelect = state.selected.concat(action.selected)
       return {...state, selected: updatedSelect}
+    case DESELECT_FROM_FRIDGE:
+      let itemToRemove = state.selected.indexOf(action.selected)
+      if (itemToRemove === -1) {
+        return {...state}
+      } else {
+        state.selected.splice(itemToRemove, 1)
+        return {...state}
+      }
     default:
       return state;
   }
