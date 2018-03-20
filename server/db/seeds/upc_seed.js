58c3aa2dfdfdbaf21b01acdb9d8e8d5699c2a358
Zubin R Dodhi <32475171+DODHI5@users.noreply.github.com>
2018-03-19 19:16:02 -1000

    created seeded data for barcodes (#32)

---
+++ server/db/seeds/upc_seed.js
@@ -0,0 +1,13 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex("upc")
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex("upc").insert([
+        { upc_code: "0051000000118", item_name: "Tomato Soup" },
+        { upc_code: "0047325023178", item_name: "Elbow Macaroni" },
+        { upc_code: "0033383660004", item_name: "Carrots" }
+      ]);
+    });
+};
--- src/actions/BarcodeAction.js
+++ src/actions/BarcodeAction.js
@@ -1,28 +1,21 @@
 import axios from "axios";
 const config = require("../../src/config");
 
-export const UPC_CODE = "https://api.nal.usda.gov/ndb/search/?format=json&q=";
-export const NDBNO_CODE = "https://api.nal.usda.gov/ndb/V2/reports?ndbno=";
+// export const UPC_CODE = "https://api.nal.usda.gov/ndb/search/?format=json&q=";
+// export const NDBNO_CODE = "https://api.nal.usda.gov/ndb/V2/reports?ndbno=";
 export const UPC_INGREDIENT = "UPC_INGREDIENT";
 
 export const getByUpc = upc => {
-  upc = parseFloat(upc);
+  console.log(upc);
   return dispatch => {
     return axios
-      .get(`${UPC_CODE}${upc}&${config.api.upc}`)
+      .post(`/api/upc`, { new: upc })
       .then(data => {
+        dispatch({
+          type: UPC_INGREDIENT,
+          ingredient: data.data
+        });
         console.log(data);
-        let ndbno = data.data.list.item[0].ndbno;
-        return axios
-          .get(`${NDBNO_CODE}${ndbno}&${config.api.ndbno}`)
-          .then(data => {
-            console.log(data);
-            let desc = data.data.foods[0].food.desc.name;
-            return dispatch({
-              type: UPC_INGREDIENT,
-              ingredient: desc
-            });
-          });
       })
       .catch(err => {
         console.log(err);
