1ff0f2b044c53e8a32368abdc0d9652975b25180
KrishnaP <kpat132@gmail.com>
2018-03-20 10:49:24 -1000

    fixes recipes, renders on recipe page

--- src/containers/Fridge.js
+++ src/containers/Fridge.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import SuggestIngredient from "./SuggestIngredient";
 import FridgeList from "../components/FridgeList";
+import { loadRecipes } from "../actions/ItemsAction";
 
 import { addIngredient } from "../actions/SuggestAction";
 import { loadFridge } from "../actions/FridgeAction";
@@ -12,17 +13,24 @@ import ConnectedRecipes from '../containers/RecipeContainer';
 class Fridge extends Component {
   constructor(props) {
     super(props);
+    this.handleSelected = this.handleSelected.bind(this);
   }
 
   componentDidMount() {
     this.props.loadFridge();
   }
 
+  handleSelected(event) {
+    this.props.getRecipes(this.props.fridgeSelect);
+    this.props.history.push("/recipes");
+  }
+
   render() {
 
     return (
       <div className="ParentFridgeClass">
-        <ConnectedRecipes />
+      <button onClick={this.handleSelected} type="submit">Selected</button>
+      {this.props.fridgeSelect.join(' ')}
 
         <FridgeList
           fridge={this.props.fridge} />
@@ -34,7 +42,10 @@ class Fridge extends Component {
 const mapStateToProps = state => {
   return {
     ingredients: state.suggest.ingredients,
-    fridge: state.fridge.fridge
+    fridge: state.fridge.fridge,
+    fridgeSelect: state.fridge.selected,
+    recipes: state.recipes.recipes
+
   };
 };
 
@@ -45,6 +56,9 @@ const mapDispatchToProps = dispatch => {
     },
     loadFridge: (id) => {
       dispatch(loadFridge(id))
+    },
+    getRecipes: (ingr) => {
+      dispatch(loadRecipes(ingr))
     }
   };
 };
