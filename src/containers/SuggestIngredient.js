6d5ac9e6a862250d827333d0bf931b2af6e9d409
jblau07 <jlau7248@gmail.com>
2018-03-20 19:00:47 -1000

    fixed so empty strings cannot be submitted

--- src/containers/SuggestIngredient.js
+++ src/containers/SuggestIngredient.js
@@ -67,9 +67,11 @@ class SuggestIngredient extends Component {
 
   handleOnSubmit (event) {
     event.preventDefault();
-    this.props.addIngredient(this.state.value);
-    console.log('handle', this.state.value)
-    this.setState({value: ''})
+    if (this.state.value !== '') {
+      this.props.addIngredient(this.state.value);
+      console.log('handle', this.state.value)
+      this.setState({value: ''})
+    }
   }
 
   render () {
