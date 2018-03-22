95b1e8916807c3fa7f2370f0f5edb29e6dddcda0
jblau07 <jlau7248@gmail.com>
2018-03-22 03:04:02 -1000

    added myCookbook to initialstate

--- src/reducers/cookbookReducer.js
+++ src/reducers/cookbookReducer.js
@@ -1,13 +1,17 @@
 import { POST_SAVED_RECIPES } from '../actions/ItemsAction';
+import { LOAD_COOKBOOK } from '../actions/CookbookAction';
 
 const initialState = {
-  cookbook: []
+  cookbook: [],
+  myCookbook: []
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case POST_SAVED_RECIPES:
       return { ...state, cookbook: action.payload }
+    case LOAD_COOKBOOK:
+      return {...state, myCookbook: action.myCookbook}
     default:
       return state;
   }
