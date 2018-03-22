ad3cf7abf8c8f4ae142e75108e6f3f1676166c82
KrishnaP <kpat132@gmail.com>
2018-03-22 10:33:40 -1000

    Added grilled cheese fallback, and find recipes button appears and dissapears

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -5,23 +5,28 @@ export const RecipeList = ({ recipe }) => {
   console.log("RECIPELIST", recipe);
   let data;
   if (!recipe) {
+    console.log("1no recipes");
+
     return <div />;
   } else {
-    if (recipe < 0) {
-      console.log("no recipes");
+    if (recipe < 1) {
+      console.log("2no recipes");
       recipe = [{
         recipe: 'Grilled Cheese',
-        ingredients:'2 slices bread, 1 slice American cheese, 1 tablespoon mayonnaise',
-        url:'http://www.geniuskitchen.com/recipe/grilled-cheese-diner-style-108522',
-        image: 'https://cdn.craftbeer.com/wp-content/uploads/grilledcheese1.jpg'
+        ingredients: '2 slices bread, 1 slice American cheese, 1 tablespoon mayonnaise',
+        url: 'http://www.geniuskitchen.com/recipe/grilled-cheese-diner-style-108522',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSaIWxnatKYCVTkcGXxoSSJmtKOS06HT0w-PdBO20gD0d1Hh1a'
       }]
 
       return (
         <ul className="grilledCheese">
+        
+          <h3>Hmmm, we could'nt find a recipe using your selected ingredients! </h3>
+          <h2>Might I suggest a Grilled Cheese?</h2>
           {recipe.map(recipes => {
             data = {
               name: recipes.recipe,
-              ingredients: recipes.ingredients.join(", "),
+              ingredients: recipes.ingredients,
               url: recipes.url,
               image: recipes.image
             };
@@ -30,10 +35,13 @@ export const RecipeList = ({ recipe }) => {
               <div className="grilledcheeseclass">
                 <h3>{data.name}</h3>
                 <h4>{data.ingredients}</h4>
-                <img href={data.url} src={data.image} />
                 <a href={data.url} target="_blank">
-                  Take Me To Recipe!
+                  <img src={data.image} />
                 </a>
+                <br />
+                {/* <a href={data.url} target="_blank">
+                  Take Me To Recipe!
+                </a> */}
                 <FavoriteRecipe key={recipes.id} {...data} />
               </div>
             );
@@ -41,7 +49,7 @@ export const RecipeList = ({ recipe }) => {
         </ul>
       );
 
-      
+
     } else {
       return (
         <ul className="userRecipes">
@@ -57,10 +65,13 @@ export const RecipeList = ({ recipe }) => {
               <div className="allrecipes">
                 <h3>{data.name}</h3>
                 <h4>{data.ingredients}</h4>
-                <img href={data.url} src={data.image} />
                 <a href={data.url} target="_blank">
-                  Take Me To Recipe!
+                  <img src={data.image} />
                 </a>
+                <br />
+                {/* <a href={data.url} target="_blank">
+                  Take Me To Recipe!
+                </a> */}
                 <FavoriteRecipe key={recipes.id} {...data} />
               </div>
             );
