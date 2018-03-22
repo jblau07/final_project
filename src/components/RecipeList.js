c59d7acd78e49a709cd83901454812b84683ed12
KrishnaP <kpat132@gmail.com>
2018-03-22 09:03:22 -1000

    grilled cheese

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -9,6 +9,39 @@ export const RecipeList = ({ recipe }) => {
   } else {
     if (recipe < 0) {
       console.log("no recipes");
+      recipe = [{
+        recipe: 'Grilled Cheese',
+        ingredients:'2 slices bread, 1 slice American cheese, 1 tablespoon mayonnaise',
+        url:'http://www.geniuskitchen.com/recipe/grilled-cheese-diner-style-108522',
+        image: 'https://cdn.craftbeer.com/wp-content/uploads/grilledcheese1.jpg'
+      }]
+
+      return (
+        <ul className="grilledCheese">
+          {recipe.map(recipes => {
+            data = {
+              name: recipes.recipe,
+              ingredients: recipes.ingredients.join(", "),
+              url: recipes.url,
+              image: recipes.image
+            };
+            console.log("WHAT ARE THE RECIPES", data);
+            return (
+              <div className="grilledcheeseclass">
+                <h3>{data.name}</h3>
+                <h4>{data.ingredients}</h4>
+                <img href={data.url} src={data.image} />
+                <a href={data.url} target="_blank">
+                  Take Me To Recipe!
+                </a>
+                <FavoriteRecipe key={recipes.id} {...data} />
+              </div>
+            );
+          })}
+        </ul>
+      );
+
+      
     } else {
       return (
         <ul className="userRecipes">
