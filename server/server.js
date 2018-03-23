ae56e09a1b2b710e1b9b5d630e4402bd0bc82918
KrishnaP <kpat132@gmail.com>
2018-03-23 10:50:51 -1000

    catchall

--- server/server.js
+++ server/server.js
@@ -71,10 +71,12 @@ app.get(`/`, (req, res) => {
 
 app.get('/*', (req,res)=> {
   var options = {
-    root: __dirname + '/public',
+    root: __dirname + '../public',
   };
   res.sendFile('index.html',options);
 })
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
